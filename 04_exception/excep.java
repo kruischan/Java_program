43d34ff4eb64b16b56852cdf6df28de424d3509c
chenkaicong <chenkaicong@cvte.com>
2019-08-18 16:31:58 +0800

    4th commit

--- 04_exception/excep.java
+++ 04_exception/excep.java
@@ -1,7 +1,7 @@
 
 
 
-
+/*
 public class excep {
     public static void main(String args[]) {
         int m = Integer.parseInt(args[0]);
@@ -17,6 +17,41 @@ public class excep {
             r = m/n;
         } catch (ArithmeticException e) {
             System.out.println(e);
+        }
+        
+        return r;
+    }
+}
+*/
+
+public class excep {
+    public static void main(String args[]) {
+        int m = 0;
+        int n = 0;
+        int ret = 0;
+
+        try {
+            m = Integer.parseInt(args[0]);
+            n = Integer.parseInt(args[1]);
+            ret = div(m, n);
+        } catch(ArithmeticException e) {
+            System.out.println("main: " + e);
+        } catch (RuntimeException e) { 
+            System.out.println(e);
+        }
+        System.out.println("m + n = " + ret);
+    }
+
+    public static int div(int m, int n) {
+        int r = 0;
+
+        try {
+            r = m/n;
+        } catch (ArithmeticException e) {
+            System.out.println("div: " + e);
+            throw new Exception("m_error");
+        } finally {
+            System.out.println("finally of div");
         }
         
         return r;
