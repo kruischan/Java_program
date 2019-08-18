0fcb25c8fb60c14f7cd842871aaf8d9a63fe4be6
chenkaicong <chenkaicong@cvte.com>
2019-08-18 17:41:02 +0800

    5th commit

---
+++ 05_package/1/math.java
@@ -0,0 +1,13 @@
+
+
+package a.b.c.d1;
+
+public class math {
+	public static int add(int x, int y) {
+		return (x + y);
+	}
+}
+
+
+
+
---
+++ 05_package/2/math.java
@@ -0,0 +1,13 @@
+
+package a.b.c.d2;
+
+public class math {
+	public static int add (int x, int y) {
+		return (x + y + 2);
+	}
+}
+
+
+
+
+
